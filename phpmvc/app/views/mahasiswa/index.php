867c22b3f4fa7f6a15a751cdab2d738575ab22dc
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-07 14:24:06 +0700

    selesai menambahkan flash alert untuk insert data ke db

--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -1,7 +1,17 @@
 <div class="container mt-3">
 
+
+
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
+            <?php Flasher::flash();?>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-lg-6">
         
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
             Tambah Data Mahasiswa
