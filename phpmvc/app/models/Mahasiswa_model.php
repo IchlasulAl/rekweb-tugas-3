1dd0cd06bf0058883bfc1b5e505bc803c0b777b5
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-07 16:08:27 +0700

    menambahkan fungsi ubah data mahasiswa menggunakan js

--- phpmvc/app/models/Mahasiswa_model.php
+++ phpmvc/app/models/Mahasiswa_model.php
@@ -62,4 +62,24 @@ class Mahasiswa_model {
 
    }
 
+   public function ubahDataMahasiswa($data){
+    $query = "UPDATE mahasiswa SET
+                    nama = :nama,
+                    nrp = :nrp,
+                    email = :email,
+                    jurusan = :jurusan 
+                WHERE id =:id";
+
+     $this->db->query($query);
+     $this->db->bind('nama', $data['nama']);
+     $this->db->bind('nrp', $data['nrp']);
+     $this->db->bind('email', $data['email']);
+     $this->db->bind('jurusan', $data['jurusan']);
+     $this->db->bind('id', $data['id']);
+
+     $this->db->execute();
+
+     return $this->db->rowCount();
+}
+
 }
--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -13,7 +13,7 @@
     <div class="row">
         <div class="col-lg-6">
         
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+        <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
             Tambah Data Mahasiswa
         </button>
         <br><br>
@@ -23,10 +23,13 @@
                         <li class="list-group-item ">
                         <?= $mhs['nama'];?>
                         
+                        
                         <a href="<?= BASEURL;?>/mahasiswa/hapus/<?= $mhs['id']?>" 
                         class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?');">hapus</a>
                         <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id']?>" 
-                        class="badge badge-primary float-right ml-1">detail</a>
+                        class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']?>">Ubah</a>
+                        <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id']?>" 
+                        class="badge badge-primary float-right ml-1 ">detail</a>
                         </li>
                 <?php endforeach;?>
             </ul>
@@ -43,14 +46,16 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
+
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">  
             
+            <input type="hidden" name="id" id="id">
             <div class="form-group" >
                 <label for="nama">Nama</label>
                 <input type="text" class="form-control" id="nama" name ="nama">
