769d7b547c7bf0e790f639d1628c137db8537788
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-07 07:32:38 +0700

    memulai membuat controller di file yang sudah dibuat

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -1,6 +1,9 @@
 <?php
 
 class App{
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
 
     public function __construct(){
         $url = $this->parseURL();
