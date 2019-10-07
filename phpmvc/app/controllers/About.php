b2df17939044b0fc046f629af36fe4aed99b147f
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-07 08:19:10 +0700

    selesai membuat controller untuk App.php di About.php dan Home.php

---
+++ phpmvc/app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About{
+
+    public function index($nama= "Ichlasul", $pekerjaan= " Mahasiswa"){
+        echo "Halo $nama, seorang $pekerjaan";
+    }
+
+    public function page(){
+        echo "About/page";
+    }
+}
--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -7,7 +7,36 @@ class App{
 
     public function __construct(){
         $url = $this->parseURL();
-        var_dump($url);
+        
+        //controller
+        if (file_exists('../app/controllers/'.$url[0].'.php' ) ) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/'. $this->controller.'.php';
+        $this->controller = new $this->controller;
+
+        //method
+        if (isset($url[1])) {
+            if ( method_exists($this->controller, $url[1])) {
+                $this->method = $url [1];
+                unset($url[1]);
+            }
+        }   
+
+
+
+        //params
+        if (!empty($url) ) {
+            $this->params = array_values($url);
+        }
+
+        //jalan contorller+method +param jika ada
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
+
+
     }
 
     public function parseURL(){
